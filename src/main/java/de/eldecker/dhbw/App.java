42a7a5c3f2c03799b424571a116d14d7cb96bc87
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-11-16 17:04:55 +0100

    String wird von STDIN eingelesen

--- src/main/java/de/eldecker/dhbw/App.java
+++ src/main/java/de/eldecker/dhbw/App.java
@@ -1,13 +1,53 @@
 package de.eldecker.dhbw;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+
+public class App  {
+	
+	private static Optional<String> urlEinlesen() {
+		
+        try {
+        	
+        	InputStreamReader inputStream = new InputStreamReader( System.in );
+            BufferedReader reader = new BufferedReader( inputStream );
+            System.out.print( "\nBitte geben Sie eine URL ein: " );
+            String eingabe = reader.readLine();                        
+            //reader.close();
+            
+            return Optional.of( eingabe );
+            
+        } catch (IOException ex) {
+        	
+            System.err.println( "Ein Fehler ist aufgetreten: " + ex.getMessage() );
+            return Optional.empty();
+        }
+	}
+
+	
+	/**
+	 * Einstiegspunkt der Programmausführung
+	 * 
+	 * @param args Kommandozeilenargumente, werden nicht ausgewertet
+	 */
+    public static void main( String[] args ) {
+        
+    	Optional<String> urlOptional = null;
+    	while ( true ) {
+    	
+    		urlOptional = urlEinlesen();
+    		if ( urlOptional.isEmpty() ) {
+    		
+    			System.out.println( "Programm wird beendet" );
+    			break;
+    		}    		
+    		
+    		String url = urlOptional.get();
+    		System.out.println( "URL eingegeben: \"" + url + "\"" );
+    	}
+    	
+    	System.out.println();
     }
 }
