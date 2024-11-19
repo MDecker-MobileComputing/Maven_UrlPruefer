78dc306ae4ad538761ff31b80a2359a096c7279b
MDecker-MobileComputer <mide_42b@yahoo.de>
2024-11-19 20:55:26 +0100

    dazu: package-info.java

---
+++ src/main/java/de/eldecker/dhbw/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Dieses Paket enthält die Klassen für einen einfachen URL-Checker,
+ * mit dem die Verwendung von Maven für ein Java-Projekt demonstriert
+ * werden soll. 
+ */
+package de.eldecker.dhbw;
